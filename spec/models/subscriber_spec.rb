cea51e7107b3223a15711ef41fcbb62feaa4e1cb
Guilherme Vinicius Moreira <gui.vinicius@gmail.com>
2015-08-26 19:20:49 -0300

    Creating Subscriber model and specs

--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Subscriber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "when email is already registered" do
+    let(:email) { "softkitty@gmail.com" }
+
+    before do
+      described_class.create(email: email)
+    end
+
+    it 'doesn\'t allows to register same email' do
+      expect {
+        described_class.create(email: email)
+      }.not_to change(described_class, :count)
+    end
+  end
+
 end
